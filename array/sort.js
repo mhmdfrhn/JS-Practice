29ea58210c4a9e6dd73356ff9db146d93e63d747
mhmdfrhn <frhn2707@gmail.com>
2023-05-05 10:03:55 +0700

    add sort.js

---
+++ array/sort.js
@@ -0,0 +1,6 @@
+// Sorts the array elements in ascending order and returns the value to a string
+console.log(["Jan", "Feb", "Mar", "Apr"].sort());
+// output: [ 'Apr', 'Feb', 'Jan', 'Mar' ]
+
+console.log([1, 30, 4, 1000, 101, 121].sort());
+// output : [ 1, 1000, 101, 121, 30, 4 ]
