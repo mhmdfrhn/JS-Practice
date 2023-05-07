a5d4994274854ce4c1310f9c373ccb3e814bc968
mhmdfrhn <frhn2707@gmail.com>
2023-05-07 11:12:04 +0700

    add promisify

---
+++ asynchronous/promisify.js
@@ -0,0 +1,21 @@
+const { promisify } = require("util");
+
+function getProvinces(countryId, callback) {
+  setTimeout(() => {
+    if (countryId === "id") {
+      callback(null, [
+        { id: "id-jk", name: "Jakarta" },
+        { id: "id-bt", name: "Banten" },
+        { id: "id-jr", name: "Jawa Barat" },
+      ]);
+      return;
+    }
+    callback(new Error("Country not found"), null);
+  });
+}
+
+const getProvincesPromise = promisify(getProvinces);
+
+getProvincesPromise("us")
+  .then((provinces) => console.log(provinces))
+  .catch((err) => console.log(err.message));
