8775cc194f6da47958daf9e45b1f7171d6041c23
mhmdfrhn <frhn2707@gmail.com>
2023-05-15 18:28:19 +0700

    modified

--- nodejs-practice/index.js
+++ nodejs-practice/index.js
@@ -1,41 +1,41 @@
-class Tiger {
-  constructor() {
-    this.strength = Math.floor(Math.random() * 100);
-  }
+// class Tiger {
+//   constructor() {
+//     this.strength = Math.floor(Math.random() * 100);
+//   }
 
-  growl() {
-    console.log("owooooo!");
-  }
-}
+//   growl() {
+//     console.log("owooooo!");
+//   }
+// }
 
-class Wolf {
-  constructor() {
-    this.strength = Math.floor(Math.random() * 100);
-  }
+// class Wolf {
+//   constructor() {
+//     this.strength = Math.floor(Math.random() * 100);
+//   }
 
-  howl() {
-    console.log("grrrrr!");
-  }
-}
+//   howl() {
+//     console.log("grrrrr!");
+//   }
+// }
 
-const fighting = (tiger, wolf) => {
-  if (tiger.strength > wolf.strength) {
-    tiger.growl();
-    return;
-  }
+// const fighting = (tiger, wolf) => {
+//   if (tiger.strength > wolf.strength) {
+//     tiger.growl();
+//     return;
+//   }
 
-  if (wolf.strength > tiger.strength) {
-    wolf.howl();
-    return;
-  }
-};
+//   if (wolf.strength > tiger.strength) {
+//     wolf.howl();
+//     return;
+//   }
+// };
 
-const tiger = new Tiger();
-const wolf = new Wolf();
+// const tiger = new Tiger();
+// const wolf = new Wolf();
 
 // console.log(fighting(tiger, wolf));
 
-import moment from "moment/moment.js";
-const date = moment().format("MMM Do YY");
+// import moment from "moment/moment.js";
+// const date = moment().format("MMM Do YY");
 
-console.log(date);
+// console.log(date);
